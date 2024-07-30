5adfd2714c4ef60d0eb8965b32f650b005ca4946
Eric Januzzi <erictomich@gmail.com>
2024-07-30 08:14:05 -0300

    Java - new exercise - Meal

---
+++ languages/java/basic_of_java/pr_basic_of_java_02_receitas/src/Meal.java
@@ -0,0 +1,21 @@
+interface food {
+    String getName();
+    int getCalories();
+    String getDescription();
+}
+
+class Carrot implements Food {
+    private String name;
+    private int calories;
+    private String description;
+
+    public Carrot() {
+        //...
+    }
+}
+
+public class Meal {
+    public static void main(String[] args) {
+
+    }
+}
